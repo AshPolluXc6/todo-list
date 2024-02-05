005b808a11c8fc578bdef344854b6b7b51506e62
Matheus Estevam <oliver.matheus@outlook.pt>
2024-02-05 17:40:11 -0300

    border added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UibuttonComponent } from './uibutton/uibutton.component';
+import { ProductCardComponent } from './product-card/product-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UibuttonComponent
+    UibuttonComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/uibutton/uibutton.component.ts
+++ src/app/uibutton/uibutton.component.ts
@@ -3,12 +3,12 @@ import { Component, Input, OnInit, } from '@angular/core';
 @Component({
   selector: 'idt-button',
   templateUrl: './uibutton.component.html',
-  styleUrl: './uibutton.component.css'
+  styleUrls: ['./uibutton.component.css']
 })
 export class UibuttonComponent {
 
   @Input() typeBt: 'default' | 'outline' | 'disabled' = 'default'
- 
+
   @Input() text: string = 'Buy';
 
 }
